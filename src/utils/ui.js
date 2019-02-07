ec7b70887cd9ac858600c2be529aabad2b3fbc49
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-02-07 20:46:42 -0200

    Add "ui" file with base styles and colors

---
+++ src/utils/ui.js
@@ -0,0 +1,18 @@
+export const baseStyles = `
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+`;
+
+export const colors = {
+  primary: {
+    normal: '#00695c',
+    light: '#439889',
+    dark: '#003d33',
+  },
+  secondary: {
+    normal: '#795548',
+    light: '#a98274',
+    dark: '#4b2c20',
+  },
+  grey: '#bdbdbd',
+};
