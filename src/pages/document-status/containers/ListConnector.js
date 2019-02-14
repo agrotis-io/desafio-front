16cd22240bb2ec637dbc650011d4f57f027bf1f7
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-02-13 22:44:48 -0200

    Add "bindActionCreators" to ListConnector

--- src/pages/document-status/containers/ListConnector.js
+++ src/pages/document-status/containers/ListConnector.js
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { fetchAll as fetchDocumentStatuses } from 'actions/document-status';
 import List from './List.jsx';
 
 const mapStateToProps = (state) => ({
   documentStatuses: state.documentStatuses,
-})
-
-const mapDispatchToProps = (dispatch, state) => ({
-  fetchDocumentStatuses: () => dispatch(fetchDocumentStatuses()),
 });
+
+const mapDispatchToProps = (dispatch) => (
+  bindActionCreators({
+    fetchDocumentStatuses,
+  }, dispatch)
+);
 
 export default connect(
   mapStateToProps,
